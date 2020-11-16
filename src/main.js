6522f7183a1cf10749e4a90cfdf42c9ac83bd3ef
Wuzhiyin <979446886@qq.com>
2020-11-16 18:22:15 +0800

    step04 完成tabbar路由链接的改造和路由高亮

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 // 入口文件
 import Vue from 'vue'
 
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
 // 导入扩展图标样式
@@ -10,10 +15,14 @@ import './lib/mui/css/icons-extra.css'
 import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
 
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
 // 导入 App 根组件
 import app from './App.vue'
 
 var vm = new Vue({
     el: '#app',
     render: c => c(app),
+    router // 1.4 挂载路由对象到 VM 实例上
 })
