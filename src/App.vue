f102eefb3c4957abd8a946778c328562043dfaba
Wuzhiyin <979446886@qq.com>
2020-11-16 18:15:01 +0800

    step03 完成tabbar的小图标设置

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
         <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
         <!-- 中间的 路由 router-view 区域 -->
         <!-- 底部 Tabbar 区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+                    <span class="mui-badge">0</span>
+                </span>
+                <span class="mui-tab-label">购物车</span>
+            </a>
+            <a class="mui-tab-item">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">搜索</span>
+            </a>
+        </nav>
     </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 // 入口文件
 import Vue from 'vue'
 
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
 // 按需导入 Mint-UI 中的组件   
 import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
