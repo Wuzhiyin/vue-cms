32b11aeed68b6c5f59a0edab9e95d83dd3d9c707
Wuzhiyin <979446886@qq.com>
2020-11-16 18:42:14 +0800

    step05 添加首页轮播图

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,9 +1,62 @@
 <template>
     <div>
-        <h3>HomeContainer</h3>
+        <!-- 轮播图区域 -->
+        <mt-swipe :auto="4000">
+            <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
+            <mt-swipe-item v-for="item in lunbotuList" :key="item.img">
+                <img :src="item.img" alt="">
+            </mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 <script>
+import {
+    Toast
+} from "mint-ui";
+
+export default {
+    data() {
+            return {
+                lunbotuList: [] // 保存轮播图的数组
+            };
+        },
+        created() {
+            this.getLunbotu();
+        },
+        methods: {
+            getLunbotu() {
+                // 获取轮播图数据的方法
+                this.$http.get("http://liulongbin.top:3005/api/getlunbo").then(result => {
+                    // console.log(result.body);
+                    if (result.body.status === 0) {
+                        // 成功了
+                        this.lunbotuList = result.body.message;
+                    } else {
+                        // 失败的
+                        Toast("加载轮播图失败。。。");
+                    }
+                });
+            }
+        }
+};
 </script>
 <style lang="scss" scoped>
+.mint-swipe {
+    height: 200px;
+    .mint-swipe-item {
+        &:nth-child(1) {
+            background-color: red;
+        }
+        &:nth-child(2) {
+            background-color: blue;
+        }
+        &:nth-child(3) {
+            background-color: cyan;
+        }
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
 </style>
