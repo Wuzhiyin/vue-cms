de2641143bf28c223098e2cbaa1d190ce1361641
Wuzhiyin <979446886@qq.com>
2020-11-17 20:39:08 +0800

    step11 改造新闻列表跳转到新闻详情的路由

---
+++ src/components/news/NewsInfo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        新闻详情 ---- {{ id }}
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id, // 将 URL 地址中传递过来的 Id值，挂载到 data上，方便以后调用
+            newsinfo: {} // 新闻对象
+        };
+    }
+};
+</script>
+<style lang="scss">
+</style>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -16,6 +17,7 @@ var router = new VueRouter({
         { path: '/shopcar', component: ShopcarContainer },
         { path: '/search', component: SearchContainer },
         { path: '/home/newslist', component: NewsList },
+        { path: '/home/newsinfo/:id', component: NewsInfo }
     ],
     linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
