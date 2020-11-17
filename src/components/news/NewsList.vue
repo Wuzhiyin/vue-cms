18e27bd41767bf7888421403f1a069dfcb7aa3f9
Wuzhiyin <979446886@qq.com>
2020-11-17 20:25:13 +0800

    step08 改造新闻资讯的路由链接

---
+++ src/components/news/NewsList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>新闻列表
+    </div>
+</template>
+<script>
+import {
+    Toast
+} from "mint-ui";
+
+export default {
+    data() {
+        return {
+            newslist: [] // 新闻列表
+        };
+    }
+};
+</script>
+<style lang="scss" scoped>
+</style>
