6a5667b5ca199008469df7d78ce4c2801a0ea70d
Wuzhiyin <979446886@qq.com>
2020-11-19 19:35:27 +0800

    step20 渲染分类列表的数据

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -4,30 +4,9 @@
         <div id="slider" class="mui-slider">
             <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
                 <div class="mui-scroll">
-                    <div id="slider" class="mui-slider">
-                        <div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
-                            <div class="mui-scroll">
-                                <a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-              推荐
-            </a>
-                                <a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-              热点
-            </a>
-                                <a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-              北京
-            </a>
-                                <a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-              社会
-            </a>
-                                <a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-              娱乐
-            </a>
-                                <a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-              科技
-            </a>
-                            </div>
-                        </div>
-                    </div>
+                    <a :class="['mui-control-item', item.id == 0 ? 'mui-active' : '']" v-for="item in cates" :key="item.id">
+            {{ item.title }}
+          </a>
                 </div>
             </div>
         </div>
@@ -39,7 +18,12 @@ import mui from "../../lib/mui/js/mui.min.js";
 
 export default {
     data() {
-            return {};
+            return {
+                cates: [], // 所有分类的列表数组
+            };
+        },
+        created() {
+            this.getAllCategory();
         },
         mounted() {
             // 当 组件中的DOM结构被渲染好并放到页面中后，会执行这个 钩子函数
@@ -49,7 +33,21 @@ export default {
                 deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
             });
         },
-        methods: {}
+        methods: {
+            getAllCategory() {
+                // 获取所有的图片分类
+                this.$http.get("api/getimgcategory").then(result => {
+                    if (result.body.status === 0) {
+                        // 手动拼接出一个最完整的 分类列表
+                        result.body.message.unshift({
+                            title: "全部",
+                            id: 0
+                        });
+                        this.cates = result.body.message;
+                    }
+                });
+            },
+        }
 };
 </script>
 <style lang="scss" scoped>
