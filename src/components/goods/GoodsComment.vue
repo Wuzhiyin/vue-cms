c3f6903ee8cdadc528d5c9346dd7da4ed0216363
Wuzhiyin <979446886@qq.com>
2020-11-21 16:20:10 +0800

    step36 实现商品详情中的图文介绍和评价页面的渲染

--- src/components/goods/GoodsComment.vue
+++ src/components/goods/GoodsComment.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-        <h3>评价</h3>
+        <cmtbox :id="$route.params.id"></cmtbox>
     </div>
 </template>
 <script>
+import cmtbox from "../subcomponents/comment.vue";
+
+export default {
+    components: {
+        cmtbox
+    }
+};
 </script>
 <style lang="scss" scoped>
 </style>
