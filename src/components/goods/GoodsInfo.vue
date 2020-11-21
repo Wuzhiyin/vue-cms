05f32be70e13377bed4520becebd427c9b50282c
Wuzhiyin <979446886@qq.com>
2020-11-21 16:32:14 +0800

    step39 每当numbox的值改变立即把购买数量传递给父组件

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -21,7 +21,7 @@
                         <del>￥{{ goodsinfo.market_price }}</del>&nbsp;&nbsp;销售价：<span class="now_price">￥{{ goodsinfo.sell_price }}</span>
                     </p>
                     <p>购买数量：
-                        <numbox></numbox>
+                        <numbox @getcount="getSelectedCount"></numbox>
                     </p>
                     <p>
                         <mt-button type="primary" size="small">立即购买</mt-button>
@@ -60,6 +60,7 @@ export default {
                 lunbotu: [], // 轮播图的数据
                 goodsinfo: {}, // 获取到的商品的信息
                 ballFlag: false, // 控制小球的隐藏和显示的标识符
+                selectedCount: 1 // 保存用户选中的商品数量， 默认，认为用户买1个
             };
         },
         created() {
@@ -137,6 +138,11 @@ export default {
             },
             afterEnter(el) {
                 this.ballFlag = !this.ballFlag;
+            },
+            getSelectedCount(count) {
+                // 当子组件把 选中的数量传递给父组件的时候，把选中的值保存到 data 上
+                this.selectedCount = count;
+                console.log("父组件拿到的数量值为： " + this.selectedCount);
             }
         },
         components: {
--- src/components/subcomponents/goodsinfo_numbox.vue
+++ src/components/subcomponents/goodsinfo_numbox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mui-numbox" data-numbox-min='1'>
         <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-        <input id="test" class="mui-input-numbox" type="number" value="1" />
+        <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox" />
         <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
     </div>
 </template>
@@ -15,7 +15,11 @@ export default {
             console.log(this.max);
         },
         methods: {
-
+            countChanged() {
+                // 每当 文本框的数据被修改的时候，立即把 最新的数据，通过事件调用，传递给父组件
+                // console.log(this.$refs.numbox.value);
+                this.$emit("getcount", parseInt(this.$refs.numbox.value));
+            }
         }
 
 
