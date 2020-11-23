7e8cb4c31d35e5b51d09cce732d76c617f34b948
Wuzhiyin <979446886@qq.com>
2020-11-23 12:26:05 +0800

    step42 实现点击加入购物车功能

--- src/main.js
+++ src/main.js
@@ -15,7 +15,31 @@ var store = new Vuex.Store({
                 // { id:商品的id, count: 要购买的数量, price: 商品的单价，selected: false  }
         },
         mutations: { // this.$store.commit('方法的名称', '按需传递唯一的参数')
+            addToCar(state, goodsinfo) {
+                // 点击加入购物车，把商品信息，保存到 store 中的 car 上
+                // 分析：
+                // 1. 如果购物车中，之前就已经有这个对应的商品了，那么，只需要更新数量
+                // 2. 如果没有，则直接把 商品数据，push 到 car 中即可
 
+                // 假设 在购物车中，没有找到对应的商品
+                var flag = false
+
+                state.car.some(item => {
+                    if (item.id == goodsinfo.id) {
+                        item.count += parseInt(goodsinfo.count)
+                        flag = true
+                        return true
+                    }
+                })
+
+                // 如果最终，循环完毕，得到的 flag 还是 false，则把商品数据直接 push 到 购物车中
+                if (!flag) {
+                    state.car.push(goodsinfo)
+                }
+
+                // 当 更新 car 之后，把 car 数组，存储到 本地的 localStorage 中
+                localStorage.setItem('car', JSON.stringify(state.car))
+            },
         },
         getters: { // this.$store.getters.***
 
